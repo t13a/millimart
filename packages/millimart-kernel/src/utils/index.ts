2101c20973ca46855088b6318abe04b37008c858
Tatsuya Kitagawa <tatsuya.kitagawa@t13a.com>
2024-05-10 22:32:04 +0900

    [kernel] Add missing imports

--- packages/millimart-kernel/src/utils/index.ts
+++ packages/millimart-kernel/src/utils/index.ts
@@ -1,5 +1,7 @@
 export * from "./actor";
+export * from "./dispatcher";
 export * from "./eventbus";
 export * from "./eventstore";
 export * from "./misc";
 export * from "./queue";
+export * from "./reducer";
